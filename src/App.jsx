112ad84218e30ecba7c5d46ea542d9bef96a2456
Hamza Ashraf <hamza.ashraf@live.co.uk>
2023-10-27 20:48:43 +0100

    reset app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello World!
+      <h1 className="text-3xl font-bold text-center p-4 font-mono text-blue-500">
+        CV Builder!
       </h1>
     </>
   )
