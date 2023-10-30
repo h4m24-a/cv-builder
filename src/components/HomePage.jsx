3d2967962f7f6c73e44bb2ea35e94c7d5b1895a0
Hamza Ashraf <hamza.ashraf@live.co.uk>
2023-10-30 21:26:02 +0000

    created layout of page with two sections

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,27 +1,30 @@
 import React from "react";
 
 const HomePage = () => {
-    return (
-        <div className="flex h-screen">
-          {/* Left Card */}
-          <div className="flex-1 bg-white p-8">
-            {/* left card content */}
-            <div className="p-4 rounded border border-gray-300">
-              <h2 className="text-2xl font-semibold">Page 1</h2>
-              {/* Your content for the first card */}
-            </div>
-          </div>
-    
-          {/* Right Card */}
-          <div className="flex-1 bg-white p-8">
-            {/* Your right card content */}
-            <div className="p-4 rounded border border-gray-300">
-              <h2 className="text-2xl font-semibold">Page 2</h2>
-              {/* Your content for the second card */}
-            </div>
+  return (
+    <>
+      <h1 className=" text-4xl font-bold font-mono text-center p-4">CV Builder</h1>
+      <div className="flex h-screen">
+        {/* Left Page */}
+        <div className=" flex-1 bg-white p-8">
+          <div className=" p-4 rounded border border-black ">
+            <h2 className="text-2xl font-semibold">Page 1</h2>
+                {/* Content here */}
+            
           </div>
         </div>
-      );
-    };
+
+        {/* Left Page */}
+        <div className=" flex-1 bg-white p-8">
+            <div className=" p-4 rounded border border-black">
+                <h2 className="text-2xl font-semibold"> Page 2</h2>
+                {/* Content here */}
+            </div>
+        </div>
+
+      </div>
+    </>
+  );
+};
 
 export default HomePage;
