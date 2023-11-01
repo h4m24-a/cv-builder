d8ce1e5b637e0cb249eae84ea04f12d785364f86
Hamza Ashraf <hamza.ashraf@live.co.uk>
2023-11-01 17:38:58 +0000

    created components

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,6 +4,7 @@ const HomePage = () => {
   return (
     <>
       <h1 className=" text-4xl font-bold font-mono text-center p-4">CV Builder</h1>
+
       <div className="flex h-screen">
         {/* Left Page */}
         <div className=" flex-1 bg-white p-8">
@@ -14,7 +15,7 @@ const HomePage = () => {
           </div>
         </div>
 
-        {/* Left Page */}
+        {/* Right Page */}
         <div className=" flex-1 bg-white p-8">
             <div className=" p-4 rounded border border-black">
                 <h2 className="text-2xl font-semibold"> Page 2</h2>
