bc772a6e13067c6a6eaf2007cb167507b85b10e1
Hamza Ashraf <hamza.ashraf@live.co.uk>
2023-11-06 22:17:14 +0000

    added input form for profile section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import HomePage from "./components/HomePage"
+import React from "react"
+import { useState } from "react"
+import Profile from "./components/Profile"
+import Education from "./components/Education"
+import Skills from "./components/Skills"
+import Experience from "./components/Experience"
 
 function App() {
+  
   return (
     <>
-    <HomePage />
-    
-    </>
+    <main>
+      <h1 className=" bg-black text-white text-4xl font-bold font-mono text-center p-4">
+        CV Builder
+      </h1>
+
+      <div className="h-screen sm: flex flex-col md:flex-row ">
+
+        {/* Left Page */}
+        <div className=" flex-1 bg-white p-8">
+          <div className=" p-4 rounded border border-black ">
+            <Profile />
+            <Education />
+            <Skills />
+            <Experience />
+          </div>
+        </div>
+
+
+
+
+        {/* Right Page */}
+        <div className=" flex-1 bg-white p-8">
+          <div className=" p-4 rounded border border-black">
+            <h2 className="text-2xl font-semibold"> CV Preview</h2>
+            
+          </div>
+        </div>
+      </div>
+    </main>
+  </>
   )
 }
 
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const [profileForm, setProfileForm] = useState({
+    fullname: "",
+    email: "",
+    number: "",
+  });
 
-export default Profile
+  const handleSumbit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setProfileForm((prevProfileFormData) => ({
+      ...prevProfileFormData,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <>
+      <h1 className="font-sans text-xl font-extrabold bg-slate-200 rounded p-2 text-center">
+        Profile
+      </h1>
+
+      <div className="flex justify-center">
+        <form onSubmit={handleSumbit}>
+          <label
+            htmlFor="name"
+            className="flex justify-center pt-3 text-md font-medium leading-6 text-gray-900"
+          >
+            Name
+          </label>
+
+          <input
+            onChange={handleInputChange}
+            value={profileForm.fullname}
+            name="fullname"
+            id="fullname"
+            type={"text"}
+            className="rounded-md border-0 py-2 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+
+          <label
+            htmlFor="Email"
+            className="flex justify-center  pt-3 text-md font-medium leading-6 text-gray-900"
+          >
+            Email
+          </label>
+
+          <input
+            onChange={handleInputChange}
+            value={profileForm.email}
+            name="email"
+            id="email"
+            type={"text"}
+            className="rounded-md border-0 py-2 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+
+          <label
+            htmlFor="Number"
+            className="flex justify-center  pt-3 text-md font-medium leading-6 text-gray-900"
+          >
+            Number
+          </label>
+
+          <input
+            onChange={handleInputChange}
+            value={profileForm.number}
+            name="number"
+            id="number"
+            type={"number"}
+            className=" rounded-md border-0 mb-8 py-2 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+
+          <div className="text-center">
+            <button
+              type="submit"
+              className="mb-5 bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Profile;
